554cd544a5b2641c84a47e126af9b8b1f0d5ca47
Lakee Sivaraya <lakeesivaraya@gmail.com>
2022-01-05 09:45:55 +0000

    working on stuff

--- src/content.ts
+++ src/content.ts
@@ -1,1 +1,17 @@
-setTimeout(() => {}, 200);
+setTimeout(() => {
+  const searchButton = document?.querySelector("button.ytd-searchbox");
+
+  searchButton?.addEventListener("mousedown", (e: Event) => {
+    const event = <MouseEvent>e;
+
+    if (event.button == 1 || event.buttons == 4) {
+      const inputElement = document?.querySelector(
+        "input.ytd-searchbox"
+      ) as HTMLInputElement;
+
+      const text = inputElement.value;
+      const url = "https://www.youtube.com/results?search_query=" + text;
+      chrome.tabs.create({ url: url, active: false });
+    }
+  });
+}, 200);
