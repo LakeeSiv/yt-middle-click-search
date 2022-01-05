f72733f83f015cbbb60668b59a4ef6d71b5516dd
Lakee Sivaraya <lakeesivaraya@gmail.com>
2022-01-05 10:19:33 +0000

    decent enough popup

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,39 @@
-import { Box, Divider, Flex, Heading } from "@chakra-ui/react";
+import {
+  Box,
+  HStack,
+  Link,
+  VStack,
+  Text,
+  Divider,
+  Heading,
+} from "@chakra-ui/layout";
 import React from "react";
+import { AiFillGithub } from "react-icons/ai";
 
 const App = () => {
   return (
     <div className="App">
-      <Box w="xs" h="xs" bgColor="gray.900" p={4}>
-        <Flex direction="column" h="full" w="full">
-          <p>Hi</p>
-        </Flex>
+      <Box w="sm" h="150px" bgColor="gray.900" p={2}>
+        <Heading>YouTube Middle Click Search</Heading>
+        <Divider></Divider>
+
+        <VStack spacing="auto">
+          <Box py={2}>
+            <Text fontSize={16}>
+              Like the extension? Drop a star on Github!
+            </Text>
+          </Box>
+          <Divider></Divider>
+          <HStack spacing={1}>
+            <AiFillGithub size={15} />
+            <a
+              href="https://github.com/LakeeSiv/yt-middle-click-search"
+              target="_blank"
+            >
+              <Link>LakeeSiv/yt-middle-click-search</Link>
+            </a>
+          </HStack>
+        </VStack>
       </Box>
     </div>
   );
